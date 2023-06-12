1bf7402fb068cedcabcc3eb0d246562dda88f40e
Jagerente <JustlPains@gmail.com>
2023-06-13 05:52:25 +0700

    add sender for each event;

--- php/src/Service/PostAnalyticsService.php
+++ php/src/Service/PostAnalyticsService.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use App\Entity\ISerializable;
+use App\Entity\Post;
+use App\Entity\PostAnalytics;
+use App\Enum\PostEventsEnum;
 
 final class PostAnalyticsService extends AnalyticsService
 {
@@ -20,5 +23,116 @@ final class PostAnalyticsService extends AnalyticsService
         } catch (\Throwable $e) {
             $this->logger->error('Error during analytics event dispatch: ' . $e);
         }
+    }
+
+    public function sendModerateEvent(
+        \Symfony\Component\Security\Core\User\UserInterface $user,
+        Post $oldPost,
+        Post $post
+    ) {
+        // Configure analytics message
+        $analyticsData = new PostAnalytics();
+
+        $analyticsData->setUserId($post->getAuthor()->getId());
+        $analyticsData->setPostId($post->getId());
+        $analyticsData->setEvent(PostEventsEnum::Moderated);
+        $analyticsData->setDetails(
+            $user->getRoles()[0] .
+            " with id " .
+            $user->getId() .
+            " moderated post with id " .
+            $post->getId() .
+            ". Status changed from " .
+            $oldPost->getStatus() .
+            " to " .
+            $post->getStatus() .
+            ($post->getModeratorNote() ?
+                ". Moderator note: \"" .
+                $post->getModeratorNote() .
+                "\"." :
+                ".")
+        );
+
+        // Send analytics message
+        $this->send($analyticsData);
+    }
+
+    public function sendDeleteEvent(
+        \Symfony\Component\Security\Core\User\UserInterface $user,
+        Post $post,
+    ) {
+        // Configure analytics message
+        $analyticsData = new PostAnalytics();
+
+        $analyticsData->setUserId($user->getId());
+        $analyticsData->setPostId($post->getId());
+        $analyticsData->setEvent(PostEventsEnum::Deleted);
+        $analyticsData->setDetails(
+            $user->getRoles()[0] .
+            " with id " .
+            $user->getId() .
+            " deleted post with id " .
+            $post->getId()
+        );
+
+        // Send analytics message
+        $this->send($analyticsData);
+    }
+
+    public function sendEditEvent(
+        \Symfony\Component\Security\Core\User\UserInterface $user,
+        Post $oldPost,
+        Post $post
+    ) {
+        // Configure analytics message
+        $analyticsData = new PostAnalytics();
+        $analyticsData->setUserId($user->getId());
+        $analyticsData->setPostId($post->getId());
+        $analyticsData->setEvent(PostEventsEnum::Edited);
+        $analyticsData->setDetails(
+            $user->getRoles()[0] .
+            " with id " .
+            $user->getId() .
+            " edited post with id " .
+            $post->getId() .
+            " . Title changed from \"" .
+            $oldPost->getTitle() .
+            "\" to \"" .
+            $post->getTitle() .
+            "\"; Body changed from \"" .
+            $oldPost->getBody() .
+            "\" to \"" .
+            $post->getBody() .
+            "\"."
+        );
+
+        // Send analytics message
+        $this->send($analyticsData);
+    }
+
+    public function sendCreateEvent(
+        \Symfony\Component\Security\Core\User\UserInterface $user,
+        Post $post,
+    ) {
+        // Configure analytics message
+        $analyticsData = new PostAnalytics();
+        $analyticsData->setUserId($post->getAuthor()->getId());
+        $analyticsData->setPostId($post->getId());
+        $analyticsData->setEvent(PostEventsEnum::Created);
+        $analyticsData->setDetails(
+            $user->getRoles()[0] .
+            " with id " .
+            $user->getId() .
+            " created post with id " .
+            $post->getId() .
+            " . Title: \"" .
+            $post->getTitle() .
+            "\"; Body: \"" .
+            $post->getBody() .
+            "\"."
+        );
+
+        // Send analytics message
+        $this->send($analyticsData);
     }
 }
